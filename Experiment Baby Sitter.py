fdb9bd6fa73353ddcc74f7b41ca2e8687f0b601a
Mahbub Hasan <91788455+hasan-mahbub@users.noreply.github.com>
2023-05-15 13:17:29 -0400

    Experiment Baby Sitter.py

--- Experiment Baby Sitter.py
+++ Experiment Baby Sitter.py
@@ -3,9 +3,10 @@ import numbers
 import os
 from time import sleep
 import wmi
+import time
 
 prev_files = 0
-
+start = time.time()
 for i in range(1000):
     f = wmi.WMI()
     all_process = ' '
@@ -15,17 +16,64 @@ for i in range(1000):
         all_process += process.Name
  
     
-    if 'firefox' in all_process :
-        filelist = os.listdir(r'C:\Users\xsobu\OneDrive - George Mason University - O365 Production\Desktop\Python baby')
+    if 'chi660d' in all_process :
+        ff=time.time()-start
+        ff=ff/3600
+        from email import parser
+        import requests
+        payload = '{"text": "Software is running(Hours):"}'
+        response = requests.post(
+        'https://hooks.slack.com/services/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',
+        data = payload)
+        print(response.text)
+        payload =  {'text': '{:.2f}'.format(ff)}
+        payload = str(payload)
+        import requests
+        response = requests.post(
+        'URL',
+        data = payload)
+        print(response.text)
+        sleep(5)
+        
+        filelist = os.listdir(r'C:\Users\nelab\Desktop\Projects\4.Feb\Microfluid 2\EIS')
         current_files= len(filelist)
         print(current_files, '<-------------------------------')
+        
         if current_files - prev_files >= 1:
             from email import parser
             import requests
-            payload = '{"text": "Experiment is running"}'
+            payload = '{"text": "Number of files saved:"}'
             response = requests.post(
-            'https://hooks.slack.com/services/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',
+            'URL',
+            data = payload)
+            payload =  {'text': '{}'.format(current_files)}
+            payload = str(payload)
+            print(payload,'<----------------------')
+            import requests
+            response = requests.post(
+            'URL',
             data = payload)
             print(response.text)
             prev_files = current_files
-    sleep(60)
+
+        else:
+            from email import parser
+            import requests
+            payload = '{"text": "Files are not getting saved "}'
+            response = requests.post(
+            'URL',
+            data = payload)
+            print(response.text)
+            prev_files = current_files
+
+    else:
+        from email import parser
+        import requests
+        payload = '{"text": "Software has been terminated"}' 
+        response = requests.post(
+        'URL',
+        data = payload)
+        print(response.text)
+        sleep(5)
+
+    sleep(3600)
